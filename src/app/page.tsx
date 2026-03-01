a01776cc606edfd55ecaefad6b964a3215fc01ea
tony8515 <tony8515@gmail.com>
2026-03-01 12:36:25 -0500

    redirect root to login

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,26 +1,5 @@
-
-"use client";
-
-import Records from "./records/Records";
-import { useEffect, useState } from "react";
-import { supabase } from "@/lib/supabase";
+import { redirect } from "next/navigation";
 
 export default function Page() {
-  const [userId, setUserId] = useState<string | null>(null);
-
-  useEffect(() => {
-    async function getUser() {
-      const { data } = await supabase.auth.getUser();
-      if (data.user) setUserId(data.user.id);
-    }
-    getUser();
-  }, []);
-
-  if (!userId) return <div className="p-4">Loading...</div>;
-
-  return (
-    <div>
-      <Records userId={userId} />
-    </div>
-  );
+  redirect("/login");
 }
